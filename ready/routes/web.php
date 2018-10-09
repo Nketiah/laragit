f6244173c755968d8e80837c181e4ba5723ee65c
Nketiah <gyatabajoe@gmail.com>
2018-10-09 10:52:36 -0400

    modified again

--- ready/routes/web.php
+++ ready/routes/web.php
@@ -17,7 +17,7 @@
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/','PagesController@index');
 Route::get('about','PagesController@about');
